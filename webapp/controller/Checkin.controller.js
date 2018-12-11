037054c1201f5a910104c6e3ffb742add12de619
Lucas Erni <lucas.erni@q-perior.com>
2018-12-11 15:59:03 +0000

    upsell

--- webapp/controller/Checkin.controller.js
+++ webapp/controller/Checkin.controller.js
@@ -28,6 +28,8 @@ sap.ui.define([
 
 			var oCM = new sap.ui.model.json.JSONModel();
 			this.getView().setModel(oCM, "CM");
+			this.getView().setModel(new sap.ui.model.json.JSONModel(), "initCM");
+			this.getView().setModel(new sap.ui.model.json.JSONModel(), "upCM");
 
 			var oFM = new sap.ui.model.json.JSONModel();
 			this.getView().setModel(oFM, "FM");
@@ -40,7 +42,7 @@ sap.ui.define([
 
 			var oModel = this.getView().getModel("RM").getData();
 			var iCategoryId = oModel.KATEGORIE_ID;
-			
+
 			if (this.getView().getModel("CM").getData()) {
 				this.getCarsByCategory(iCategoryId, "CM");
 			}
@@ -57,51 +59,85 @@ sap.ui.define([
 			var oModel = this.getView().getModel("RM").getData();
 			var iCategoryId = oModel.KATEGORIE_ID;
 			if (iId === 2) {
-				this.getCarsByCategory(iCategoryId, "CM");
-				var oCars = this.getView().getModel("CM").getData();
 
-				var oResultSet = oCars.results;
-				if (this.getView().byId("avatar0").getProperty("title") === "") {
-					// create avatar and text
-					for (var i = 0; i < oResultSet.length; i++) {
-						var j = oResultSet[i].PRODUKT_ID;
-						var oAvatar = new sap.f.Avatar({
-							src: "img/" + j + ".jpg",
-							displayShape: "Square",
-							displaySize: "Custom",
-							customDisplaySize: "200px",
-							press: function () {
-								alert("Hello");
-							}
-						});
+				this.getCarsByCategory(iCategoryId, "initCM");
+				var oInitialCars = this.getView().getModel("initCM").getData();
+				var oInitialResultSet = oInitialCars.results;
 
-						var sText = oResultSet[i].BESCHREIBUNG.substring(0, 200) + "...";
-						var oText = new sap.m.Text({
-							text: sText,
-							width: "200px",
-							press: function() {
-								alert("Hello");
-							}
-						});
-						
-						var oButton = new sap.m.Button({
-							text: "Dieses Fahrzeug auswählen!",
-							press: function() {
-								alert("Hello");
-							}
-						});
+				this.getCarsByCategory(iCategoryId + 1, "upCM");
+				var oUpsellCars = this.getView().getModel("upCM").getData();
+				var oUpsellResultSet = oUpsellCars.results;
 
-						// placing it into our view
-						this.getView().byId("avatar" + i).addContent(oAvatar);
-						this.getView().byId("avatar" + i).addContent(oText);
-						this.getView().byId("avatar" + i).setTitle(oResultSet[i].BEZEICHNUNG);
-						this.getView().byId("avatar" + i).addContent(oButton);
-					}
+				//if (this.getView().byId("avatar0").getProperty("title") === "") {
+				// create avatar and text
+				for (var i = 0; i < oInitialResultSet.length; i++) {
+					self.addInfosToTiles(oInitialResultSet, i, "initialCarRow", false);
 				}
-			}
-			this.getView().byId("tab" + iId).setEnabled(true);
-			this.getView().byId("idIconTabBar2").setSelectedKey("tab" + iId);
 
+				for (var x = 0; x < oUpsellResultSet.length; x++) {
+					self.addInfosToTiles(oUpsellResultSet, x, "upsellCarRow", true);
+				}
+
+				//}
+				this.getView().byId("tab" + iId).setEnabled(true);
+				this.getView().byId("idIconTabBar2").setSelectedKey("tab" + iId);
+			}
+		},
+
+		addInfosToTiles: function (oResultSet, iIndex, oElement, bIsUpselling) {
+			var self = this;
+			var i = iIndex;
+			var j = oResultSet[i].PRODUKT_ID;
+
+			var iKategorieId = oResultSet[i].KATEGORIE_ID;
+
+			var oBlockLayout = new sap.ui.layout.BlockLayoutCell({
+			});
+
+			var oAvatar = new sap.f.Avatar({
+				src: "img/" + j + ".jpg",
+				displayShape: "Square",
+				displaySize: "Custom",
+				customDisplaySize: "200px",
+				press: function () {
+					alert("Hello");
+				}
+			});
+
+			var sText = oResultSet[i].BESCHREIBUNG.substring(0, 200) + "...";
+			var oText = new sap.m.Text({
+				text: sText,
+				width: "200px",
+				press: function () {
+					alert("Hello");
+				}
+			});
+
+			var oButton = new sap.m.Button({
+				text: "Dieses Fahrzeug wählen",
+				press: function () {
+					alert("Hello");
+				}
+			});
+			if (bIsUpselling) {
+				var iPreisAlt = this.getPricePerCategory(iKategorieId -1);
+				var iPreisNeu = this.getPricePerCategory(iKategorieId);
+				var iAdd = (iPreisNeu - iPreisAlt);
+				oButton = new sap.m.Button({
+					text: "Diese Fahrzeug wählen für +" + iAdd,
+					press: function () {
+						alert("Hello");
+					}
+				});
+
+			}
+
+			// placing it into our view
+			this.getView().byId(oElement).addContent(oBlockLayout);
+			oBlockLayout.addContent(oAvatar);
+			oBlockLayout.addContent(oText);
+			oBlockLayout.setTitle(oResultSet[i].BEZEICHNUNG);
+			oBlockLayout.addContent(oButton);
 		},
 
 		getUserData: function (iId) {
@@ -109,8 +145,7 @@ sap.ui.define([
 				success: function (oData) {
 					this.getView().getModel("PM").setData(oData);
 				}.bind(this),
-				error: function (e) {
-				}
+				error: function (e) {}
 			});
 		},
 
@@ -145,8 +180,7 @@ sap.ui.define([
 				success: function (oData) {
 					this.getView().getModel(sModel).setData(oData);
 				}.bind(this),
-				error: function (e) {
-				}
+				error: function (e) {}
 			});
 		},
 
@@ -156,8 +190,17 @@ sap.ui.define([
 				success: function (oData) {
 					this.getView().getModel(sModel).setData(oData);
 				}.bind(this),
-				error: function (e) {
-				}
+				error: function (e) {}
+			});
+		},
+
+		getPricePerCategory: function (iId) {
+			this.getView().getModel().read("/KATEGORIE", {
+				filters: [new sap.ui.model.Filter("KATEGORIE_ID", sap.ui.model.FilterOperator.EQ, iId)],
+				success: function (oData) {
+					return oData.results[0].PREIS;
+				}.bind(this),
+				error: function (e) {}
 			});
 		},
 
@@ -166,9 +209,11 @@ sap.ui.define([
 			var iId = parseInt(sId.slice(-1), 10) - 1;
 
 			if (sId === "tab1") {
-				this.getView().byId("avatar0").removeAllContent();
-				this.getView().byId("avatar1").removeAllContent();
+				this.getView().byId("upsellCarRow").removeAllContent();
+				this.getView().byId("initialCarRow").removeAllContent();
 				this.getView().getModel("CM").setData(null);
+				this.getView().getModel("initCM").setData(null);
+				this.getView().getModel("upCM").setData(null);
 				this.getView().byId("avatar0").setTitle("");
 				this.getView().byId("avatar1").setTitle("");
 				this.getRouter().getTargets().display("Startpage", {
